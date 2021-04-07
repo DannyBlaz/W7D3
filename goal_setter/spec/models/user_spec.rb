bc6c57cc22c9fe98ef1219c7945129068d0da394
Daniel <dannyblaze469@gmail.com>
2021-04-07 15:45:19 -0400

    Added validations for password and password digest

--- goal_setter/spec/models/user_spec.rb
+++ goal_setter/spec/models/user_spec.rb
@@ -16,4 +16,22 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:session_token) }
 
+  describe '#password=' do
+    #SHOULD BE TAKING in a password 
+    #set password digest using bcrypt
+    #@password = password
+    let(:user) {User.new(username: 'daniel')}
+
+    
+    it "sets @password = password" do 
+      user.password= "password"
+      expect(user.password).to eq("password")
+    end
+
+    it "should set user.password_digest" do
+      user.password = "password"
+      expect(BCrypt::Password.new(user.password_digest).is_password?("password")).to be true
+    end
+  end
+
 end
