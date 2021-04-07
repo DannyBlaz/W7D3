c74d38f1681515b8f6de38171477e9d24e920adf
Daniel <dannyblaze469@gmail.com>
2021-04-07 15:01:34 -0400

    Created factory method

--- goal_setter/spec/models/user_spec.rb
+++ goal_setter/spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  before(:each) do
+    user = User.new(username: 'daniel', session_token: "bkaubyd8lluhdsy")
+  end
+
   #validations
   it { should validate_presence_of(:username) }
   it { should validate_uniqueness_of(:username) }
